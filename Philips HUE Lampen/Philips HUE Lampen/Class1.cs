7d26e473fa2f8b3a87fbac4d9555f2ea3c760334
davidVeer <151018675+davidVeer@users.noreply.github.com>
2024-12-17 16:43:30 +0100

    some random stuff

---
+++ Philips HUE Lampen/Philips HUE Lampen/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Philips_HUE_Lampen
+{
+    public class State
+    {
+        public int[] xy { get; set; }
+        public int ct { get; set; }
+        public string alert { get; set; }
+        public int sat { get; set; }
+        public string effect { get; set; }
+        public int bri { get; set; }
+        public int hue { get; set; }
+        public string colormode { get; set; }
+        public bool reachable { get; set; }
+        public bool on { get; set; }
+    }
+
+}
--- Philips HUE Lampen/Philips HUE Lampen/Layouts/Connection.xaml.cs
+++ Philips HUE Lampen/Philips HUE Lampen/Layouts/Connection.xaml.cs
@@ -1,10 +1,38 @@
-﻿namespace Philips_HUE_Lampen
+﻿using System.Net.Http.Json;
+using System.Text;
+
+namespace Philips_HUE_Lampen
 {
     public partial class Connection : ContentPage
     {
         public Connection()
         {
             InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Task.Run(async ()=> SendHTTPRequest("localhost", "newdeveloper", "1" , "{\"on\": false}"));
+        }
+
+        public static async Task SendHTTPRequest(string bridgeIp, string username, string lampId, string jsonPayload)
+        {
+            HttpClient client = new HttpClient();
+            try
+            {
+                string url = $"http://{bridgeIp}/api/{username}/lights/{lampId}/state";
+                var content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+                var response = await client.PutAsync(url, content);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"Response: {responseBody}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Fout bij het versturen van het commando: {e.Message}");
+            }
+
+
         }
     }
 }
