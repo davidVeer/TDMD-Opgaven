2003619de4e0d8612e6e8e1b8c93505331a11450
Erik <er.dijkstra@student.avans.nl>
2024-12-22 14:08:00 +0100

    weird

---
+++ Philips HUE Lampen/Philips HUE Lampen/Layouts/ConnectedLights.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Philips_HUE_Lampen
+{
+    public partial class ConnectedLamps : ContentPage
+    {
+        public ConnectedLamps()
+        {
+            InitializeComponent();
+
+            listView.ItemsSource = APIConnection.list;
+        }
+
+    }
+
+}
